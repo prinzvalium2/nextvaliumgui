918ddd4fbd0bab0465ae048f37226f1dc27679a5
Prinz Valium II. <spammails@turboprinz.de>
2019-11-03 16:55:16 +0100

    bugfix calculation uranium consumption

--- src/main/java/de/prinzvalium/nextvaliumgui/gui/dialog/planetdetails/PanelFleet.java
+++ src/main/java/de/prinzvalium/nextvaliumgui/gui/dialog/planetdetails/PanelFleet.java
@@ -970,7 +970,7 @@ public class PanelFleet extends JPanel {
             double distance = Math.sqrt(distX*distX + distY*distY);
             
             String selectedMission = (String)comboBoxMissionsStandard.getSelectedItem();
-            if (selectedMission.equalsIgnoreCase(MISSION_TRANSPORT))
+            if (!selectedMission.equalsIgnoreCase(MISSION_EXPLORE))
                 distance += distance;
             
             doubleConsumption = distance * consumptionTotal;

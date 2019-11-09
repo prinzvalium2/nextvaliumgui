d1b5461084e33b8bcb88f439e3c13e8550624f18
Prinz Valium II. <spammails@turboprinz.de>
2019-11-09 16:54:03 +0100

    Revert "df"
    This reverts commit 56dc6b18e8b03405b0cc6cc85519659d16f5d935.

--- src/main/java/de/prinzvalium/nextvaliumgui/gui/PanelFlightRadar.java
+++ src/main/java/de/prinzvalium/nextvaliumgui/gui/PanelFlightRadar.java
@@ -2,6 +2,7 @@ package de.prinzvalium.nextvaliumgui.gui;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.geom.Line2D;
 import java.util.Collection;
 

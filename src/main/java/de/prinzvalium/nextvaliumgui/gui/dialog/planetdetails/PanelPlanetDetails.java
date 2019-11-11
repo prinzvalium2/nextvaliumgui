b93773ce04ce794079c2f9d4f38855d135a560d8
Prinz Valium II. <spammails@turboprinz.de>
2019-11-11 14:43:31 +0100

    fasten up dialog planet details

--- src/main/java/de/prinzvalium/nextvaliumgui/gui/dialog/planetdetails/PanelPlanetDetails.java
+++ src/main/java/de/prinzvalium/nextvaliumgui/gui/dialog/planetdetails/PanelPlanetDetails.java
@@ -6,6 +6,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 
 import de.prinzvalium.nextvaliumgui.nextcolony.Planet;
 import de.prinzvalium.nextvaliumgui.nextcolony.PlanetDetails;
@@ -16,6 +17,7 @@ import java.awt.GridBagConstraints;
 public class PanelPlanetDetails extends JPanel {
 
     private static final long serialVersionUID = 1L;
+    private JLabel panelImage;
     
     public PanelPlanetDetails(Planet planet) {
         
@@ -26,22 +28,33 @@ public class PanelPlanetDetails extends JPanel {
         gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
         setLayout(gridBagLayout);
         
-        JLabel panelImage = new JLabel();
-        try {
-            PlanetDetails planetDetails = new PlanetDetails(planet.getId());
-            String path = "https://nextcolony.io/img/planets/" + planetDetails.getImg();
-            URL url = new URL(path);
-            BufferedImage image = ImageIO.read(url);
-            panelImage = new JLabel(new ImageIcon(image));
-        }
-        catch (Exception e) {
-        }
+        panelImage = new JLabel();
       
         GridBagConstraints gbc_panelImage = new GridBagConstraints();
         gbc_panelImage.fill = GridBagConstraints.BOTH;
         gbc_panelImage.gridx = 0;
         gbc_panelImage.gridy = 0;
-        add(panelImage, gbc_panelImage);
         
+        add(panelImage, gbc_panelImage);
+       
+        new SwingWorker<Object, Object>() {
+
+            @Override
+            protected Object doInBackground() throws Exception {
+                PlanetDetails planetDetails = new PlanetDetails(planet.getId());
+                String path = "https://nextcolony.io/img/planets/" + planetDetails.getImg();
+                URL url = new URL(path);
+                BufferedImage image = ImageIO.read(url);
+                panelImage = new JLabel(new ImageIcon(image));
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                add(panelImage, gbc_panelImage);
+                super.done();
+            }
+            
+        }.execute();
     }
 }

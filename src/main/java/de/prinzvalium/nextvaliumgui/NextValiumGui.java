402021b06a55e0e953bda1fc6c93b8cae8056fb4
Prinz Valium II. <spammails@turboprinz.de>
2020-04-18 10:20:10 +0200

    Version 0.11.0

--- src/main/java/de/prinzvalium/nextvaliumgui/NextValiumGui.java
+++ src/main/java/de/prinzvalium/nextvaliumgui/NextValiumGui.java
@@ -72,7 +72,7 @@ import javax.swing.event.ChangeEvent;
 public class NextValiumGui {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NextValiumGui.class);
-    private static final String version = "0.10.1-SNAPSHOT";
+    private static final String version = "0.11.0";
     private static NextValiumGui nextValiumGui = null;
     private JFrame frmNextvaliumManagementGui;
     private JTextField textFieldPosX;
